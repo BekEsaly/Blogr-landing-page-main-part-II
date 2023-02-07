cd0b6b7fe68022b7211658296e1fafe9226a8c9c
BekEsaly <85221780+BekEsaly@users.noreply.github.com>
2023-02-07 19:43:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const ddBtn=document.querySelectorAll('.dropdown-btn');
+const ddMenu=document.querySelectorAll('.dropdown-menu');
+const arrow=document.querySelectorAll('.dropdown-btn a img');
+ddBtn.forEach((btn, current)=>{
+    btn.addEventListener('click', (actual, arrowUp)=>{
+         actual=ddMenu[current];
+        arrowUp=arrow[current];
+            ddMenu.forEach((e)=>{
+               e.classList.remove('active') 
+            });
+             arrow.forEach((e)=>{
+               e.classList.remove('arrow-up') 
+            });
+           actual.classList.toggle('active');
+            arrowUp.classList.toggle('arrow-up');
+    })
+}) 
+document.addEventListener("click", (e) => {
+		if (!e.target.closest('.header--menu-link')) {
+	    ddMenu.forEach((e)=>{
+               e.classList.remove('active') 
+            });
+             arrow.forEach((e)=>{
+               e.classList.remove('arrow-up') 
+            })
+		}
+})
+const hamburger=document.querySelector('.hamburger');
+const menu=document.querySelector('.header--menu-link');
+ 
+hamburger.addEventListener('click', ()=>{
+ hamburger.classList.toggle('icon-close');
+ menu.classList.toggle('hamburger-active')
+  
+})
